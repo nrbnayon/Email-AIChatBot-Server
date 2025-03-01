a993fd3fc052ae3bba81e2c6ceb9cb028575c4b6
nrbnayon <nrbnayon@gmail.com>
2025-03-01 13:56:46 +0600

    Vecel json

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.use((req, res, next) => {
 });
 
 app.get("/", (req, res) => {
-  res.send("Hello from how can i help you!");
+  res.send("Hello developer! How can I help you?");
 });
 
 // Routes
